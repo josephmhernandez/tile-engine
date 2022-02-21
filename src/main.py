4bfaaac3d11e5271827ccefce3c67eb0cd8bc479
Joseph Hernandez <joseph@macbook-air.myfiosgateway.com>
2022-02-21 06:30:31 -0500

    tile engine at tiles 512 x 512

---
+++ src/main.py
@@ -0,0 +1,56 @@
+# Tile Engine donwload, assemble, and print tiles into a large image file
+
+# Input: 
+# - BBox (EPSG:4326 projected coordinates) [float] size 4
+# - Height (inches) of final picture
+# - Width (inches) of final picture 
+# - Size of tiles we want to download (256, 512, 1024) 
+
+# Output: 
+# - Tiff Image of map at high resolution > 300 dpi
+
+from coord import Coord
+from downloader import Downloader
+from assembler import Assembler
+from bbox import Bbox
+
+import PIL.Image
+
+def test_crop():
+    img = PIL.Image.open('./downloaded_image.png')
+    crop_payload = (0 + 100, 0, img.width-100, img.height)
+    im1 = img.crop(crop_payload)
+    im1.save('.output_test.png')
+
+    if(im1.size == img.size):
+        print('good')
+    pass
+
+if __name__ == "__main__":
+    # 1. Use BBox & size of tiles to get the List of tiles we want to download 
+    # 2. Multi threaded download of tiles
+    # 3. Assemble tiles on download completion
+    # 4. Output tiff / Download tiff to device / store tiff somewhere? 
+    
+    bbox = [-77.09690093994142,38.86042928143244,-76.97673797607423,38.95393580081098]
+
+    tile_size = 0
+    base_url = 'https://api.maptiler.com/maps/voyager/256/{z}/{x}/{y}@2x.png?key=msqCXSKnPannMXvUCPsn#' 
+
+    zoom = 15
+    
+    # bbox = [-77.15698242187501,38.81376360182924,-76.91665649414062,39.00077626698007]
+    map_box = Bbox(Coord(bbox[0], bbox[3]), Coord(bbox[2], bbox[1]))
+
+    new_grid = Downloader.generate_tile_lists(map_box, tile_size, base_url, zoom) 
+    
+    # grid = [4,3]
+    print('new_grid')
+    # print(new_grid)
+    Assembler.assemble_image('./src/tile_images/', new_grid, 'downloaded_image')
+
+
+    Assembler.crop_image(map_box, './downloaded_image.png', output_path="./cropped_image.png", zoom=zoom)
+    # test_crop()
+
+
---
+++ test/downloader_test.py
@@ -0,0 +1,11 @@
+import pytest   
+
+# Note: Test Classes must start with "Test"
+# Note: Test Functions must start with "test"
+
+from downloader import Downloader
+
+class TestDowloader:
+    
+    def test_simple(self):
+        assert (2 + 2 != 1)
