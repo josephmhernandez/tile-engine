38c4333de724e45d9fdae841892daedfbe0899e2
Joseph Hernandez <josephmhernandez15@gmail.com>
2023-02-14 23:30:28 -0500

    convert bbox to mercantile web xy units before calculations so we get correct crop & pin location

--- src/models/coord.py
+++ src/models/coord.py
@@ -1,10 +1,11 @@
-
-
 from pickletools import long1
 import decimal
 import numpy as np
+import mercantile
+
+
 class Coord:
-    def __init__(self, lon : decimal, lat : decimal):
+    def __init__(self, lon: decimal, lat: decimal):
         self.lon = decimal.Decimal(lon)
         self.lat = decimal.Decimal(lat)
 
@@ -16,3 +17,12 @@ class Coord:
 
     def __str__(self):
         return "lon: " + str(self.lon) + ", lat: " + str(self.lat)
+
+    def get_web_mercator(self) -> tuple:
+        return mercantile.xy(lng=self.lon, lat=self.lat)
+
+    def get_web_mercator_x(self) -> float:
+        return mercantile.xy(lng=self.lon, lat=self.lat)[0]
+
+    def get_web_mercator_y(self) -> float:
+        return mercantile.xy(lng=self.lon, lat=self.lat)[1]
